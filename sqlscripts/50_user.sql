f07040f2e1f31c1814ed8eb12804e90d7a336a70
pix303 <303pix@gmail.com>
2025-10-20 11:05:47 +0200

    feat(sql): add user table

--- sqlscripts/50_user.sql
+++ sqlscripts/50_user.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS locale.user(
   subject_id varchar(128) NOT NULL,
   email varchar(128) NOT NULL,
   name varchar(128) NOT NULL,
+  picture varchar(512),
   role int NOT NULL DEFAULT 2, -- translator role
   contexts varchar(64)[],
     CONSTRAINT user_pkey PRIMARY KEY (subject_id )
